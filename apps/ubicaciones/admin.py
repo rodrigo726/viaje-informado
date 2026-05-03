86357f948d0f603a7e94c8a526392d1aaed45d08
Rodrigo <rodrigo_6_26@hotmail.com>
2026-05-03 12:53:19 -0500

    Se crearon los modelos e importador de ubicaciones

--- apps/ubicaciones/admin.py
+++ apps/ubicaciones/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Departamento, Provincia, Distrito, Localidad
 
-# Register your models here.
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_oficial', 'codigo_inei', 'activo', 'creado')
+    search_fields = ('nombre_oficial', 'codigo_inei')
+    list_filter = ('activo',)
+    prepopulated_fields = {'slug': ('nombre_oficial',)}
+
+@admin.register(Provincia)
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_oficial', 'departamento', 'codigo_inei', 'activo')
+    search_fields = ('nombre_oficial', 'codigo_inei', 'departamento__nombre_oficial')
+    list_filter = ('activo', 'departamento')
+    prepopulated_fields = {'slug': ('nombre_oficial',)}
+    autocomplete_fields = ('departamento',)
+
+@admin.register(Distrito)
+class DistritoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_oficial', 'provincia', 'departamento_relacionado', 'codigo_inei', 'codigo_api', 'api_id', 'activo')
+    search_fields = ('nombre_oficial', 'codigo_inei', 'codigo_api', 'provincia__nombre_oficial', 'provincia__departamento__nombre_oficial')
+    list_filter = ('activo', 'provincia__departamento', 'provincia')
+    prepopulated_fields = {'slug': ('nombre_oficial',)}
+    autocomplete_fields = ('provincia',)
+
+    def departamento_relacionado(self, obj):
+        return obj.provincia.departamento
+    departamento_relacionado.short_description = 'Departamento'
+
+@admin.register(Localidad)
+class LocalidadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tipo', 'distrito', 'provincia_relacionada', 'departamento_relacionado', 'activo')
+    search_fields = ('nombre', 'distrito__nombre_oficial', 'distrito__provincia__nombre_oficial', 'distrito__provincia__departamento__nombre_oficial')
+    list_filter = ('activo', 'tipo', 'distrito__provincia__departamento', 'distrito__provincia', 'distrito')
+    prepopulated_fields = {'slug': ('nombre',)}
+    autocomplete_fields = ('distrito',)
+
+    def provincia_relacionada(self, obj):
+        return obj.distrito.provincia
+    provincia_relacionada.short_description = 'Provincia'
+
+    def departamento_relacionado(self, obj):
+        return obj.distrito.provincia.departamento
+    departamento_relacionado.short_description = 'Departamento'
